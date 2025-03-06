fd6b50368cc5812fbf946c9c77fbbc09a7b324a5
JuanDavid1430 <juangarzoncollazos@gmail.com>
2025-03-06 17:59:34 -0500

    Integracion Laravel en proceso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'AutoAnalytics';
+export class AppComponent implements OnInit {
+  mensaje: string = 'AJAJAJAJAJ';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getTest().subscribe(response => {
+      this.mensaje = response.message;
+    });
+  }
 }
