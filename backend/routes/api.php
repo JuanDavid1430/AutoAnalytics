617506366826f8c8f2db9fa16d0384fa278b6e28
JuanDavid1430 <juangarzoncollazos@gmail.com>
2025-03-06 21:20:37 -0500

    eliminacion carpetas innecesarias

--- backend/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/test', function () {
-    return response()->json(['message' => 'API funcionando correctamente 🚀']);
-});
