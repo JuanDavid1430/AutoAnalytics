1cec7e4ad9b6d521f96117d124185a2a1fa4177c
JuanDavid1430 <juangarzoncollazos@gmail.com>
2025-03-06 23:43:17 -0500

    agregados archivos

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Necesario para ngModel
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule], // Importa FormsModule para formularios
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  message: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        this.message = response.success ? 'Login exitoso' : 'Credenciales incorrectas';
+      },
+      error: (error) => {
+        console.error(error);
+        this.message = 'Error al intentar login';
+      }
+    });
+  }
+}
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl =  environment.apiUrl; // Ajusta la URL según tu backend
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    const body = { email, password };
+    return this.http.post(`${this.apiUrl}/login`, body);
+  }
+}
